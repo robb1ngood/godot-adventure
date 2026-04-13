072832193e477f7078c98d88b2c4181061ed891b
robb1ngood <mark.mucska@gmail.com>
2026-04-13 20:15:04 +0200

    initial

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,24 +1,12 @@
 extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var score: int = 200
-	var money: float = 15.5
-	var username: String = "Bob"
-	var is_player_alive: bool= true
-	var first_name: String = "Márk"
-	var damage: float = 7.5
-	
-	score = 20
-	
-	print(score + 200 - 100 * score)
-	print(username)
-	print(is_player_alive)
-	print(first_name)
-	print(damage)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_right"):
+		print("You're pressing right")
+	if Input.is_action_pressed("move_left"):
+		print("You're pressing left")

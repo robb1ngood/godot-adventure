934f028e12de05d8d29055d0191359493a89ec4e
robb1ngood <mark.mucska@gmail.com>
2026-09-16 11:50:57 +0200

    enemy movement

--- Scenes/Enemies/slime_enemy.gd
+++ Scenes/Enemies/slime_enemy.gd
@@ -1,10 +1,36 @@
 extends CharacterBody2D
 
+@export var speed: float = 30
+
 var target: Node2D
 
 func _physics_process(delta):
+	chase_target()
+		
+	move_and_slide()
+	
+	animate_enemy()
+
+func chase_target():
 	if target:
-		pass
+		var distance_to_player: Vector2
+		distance_to_player = target.global_position - global_position
+		
+		var direction_normal: Vector2 = distance_to_player.normalized()
+		
+		velocity = direction_normal * speed
+
+func animate_enemy():
+	var normal_velocity: Vector2 = velocity.normalized()
+	
+	if normal_velocity.x > 0.707:
+		$AnimatedSprite2D.play("move_right")
+	elif normal_velocity.x < -0.707:
+		$AnimatedSprite2D.play("move_left")
+	elif normal_velocity.y > 0.707:
+		$AnimatedSprite2D.play("move_down")
+	elif normal_velocity.y < -0.707:
+		$AnimatedSprite2D.play("move_up")
 
 func _on_player_detect_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
